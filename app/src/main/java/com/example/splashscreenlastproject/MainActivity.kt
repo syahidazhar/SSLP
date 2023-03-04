f2dd5f62db3f7be4ef472c2aa960ea723a5881d7
Deus <syahidazharazizi@gmail.com>
2023-03-04 17:53:51 +0700

    SplashS

---
+++ app/src/main/java/com/example/splashscreenlastproject/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.splashscreenlastproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window!!.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val backgroundImage : ImageView = findViewById(R.id.SplashScreenImage)
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide)
+        backgroundImage.startAnimation(slideAnimation)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
